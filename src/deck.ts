506b08ac1c1c768f048a455f85b47a51163dd19d
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-18 19:43:38 +0930

    Add detailed profile generator

--- src/deck.ts
+++ src/deck.ts
@@ -1,6 +1,26 @@
 import { Message } from "eris";
 import fetch from "node-fetch";
 import { TypedDeck, parseURL, toURL } from "ydke";
+import { data } from "./data";
+import { enums } from "ygopro-data";
+
+interface DeckProfile {
+	nameCounts: {
+		main: ProfileCounts;
+		extra: ProfileCounts;
+		side: ProfileCounts;
+	};
+	typeCounts: {
+		main: ProfileCounts;
+		extra: ProfileCounts;
+		side: ProfileCounts;
+	};
+	archetypes: string[];
+	ydk: string;
+	url: string;
+}
+
+type ProfileCounts = { [name: string]: number };
 
 export class Deck {
 	readonly url: string;
@@ -104,5 +124,134 @@ export class Deck {
 			ydk += code + "\n";
 		}
 		return ydk;
+	}
+
+	private static increment(obj: ProfileCounts, key: string): void {
+		if (key in obj) {
+			obj[key]++;
+		} else {
+			obj[key] = 1;
+		}
+	}
+
+	public async getProfile(): Promise<DeckProfile> {
+		const mainTypeCounts: ProfileCounts = {
+			monster: 0,
+			spell: 0,
+			trap: 0
+		};
+		const extraTypeCounts: ProfileCounts = {
+			fusion: 0,
+			synchro: 0,
+			xyz: 0,
+			link: 0
+		};
+		const sideTypeCounts: ProfileCounts = {
+			monster: 0,
+			spell: 0,
+			trap: 0
+		};
+
+		const mainNameCounts: ProfileCounts = {};
+		const extraNameCounts: ProfileCounts = {};
+		const sideNameCounts: ProfileCounts = {};
+
+		const archetypeCounts: ProfileCounts = {};
+
+		for (const code of this.record.main) {
+			const card = await data.getCard(code, "en");
+			if (!card) {
+				Deck.increment(mainNameCounts, code.toString());
+				continue;
+			}
+			if ("en" in card.text) {
+				Deck.increment(mainNameCounts, card.text.en.name);
+				const sets = await card.data.names.en.setcode;
+				for (const set of sets) {
+					Deck.increment(archetypeCounts, set);
+				}
+			} else {
+				Deck.increment(mainNameCounts, code.toString());
+			}
+
+			if (card.data.isType(enums.type.TYPE_MONSTER)) {
+				Deck.increment(mainTypeCounts, "monster");
+			} else if (card.data.isType(enums.type.TYPE_SPELL)) {
+				Deck.increment(mainTypeCounts, "spell");
+			} else if (card.data.isType(enums.type.TYPE_TRAP)) {
+				Deck.increment(mainTypeCounts, "trap");
+			}
+		}
+
+		for (const code of this.record.extra) {
+			const card = await data.getCard(code, "en");
+			if (!card) {
+				Deck.increment(extraNameCounts, code.toString());
+				continue;
+			}
+			if ("en" in card.text) {
+				Deck.increment(extraNameCounts, card.text.en.name);
+				const sets = await card.data.names.en.setcode;
+				for (const set of sets) {
+					Deck.increment(archetypeCounts, set);
+				}
+			} else {
+				Deck.increment(extraNameCounts, code.toString());
+			}
+
+			if (card.data.isType(enums.type.TYPE_FUSION)) {
+				Deck.increment(extraTypeCounts, "fusion");
+			} else if (card.data.isType(enums.type.TYPE_SYNCHRO)) {
+				Deck.increment(extraTypeCounts, "synchro");
+			} else if (card.data.isType(enums.type.TYPE_XYZ)) {
+				Deck.increment(extraTypeCounts, "xyz");
+			} else if (card.data.isType(enums.type.TYPE_LINK)) {
+				Deck.increment(extraTypeCounts, "link");
+			}
+		}
+
+		for (const code of this.record.side) {
+			const card = await data.getCard(code, "en");
+			if (!card) {
+				Deck.increment(sideNameCounts, code.toString());
+				continue;
+			}
+			if ("en" in card.text) {
+				Deck.increment(sideNameCounts, card.text.en.name);
+			} else {
+				Deck.increment(sideNameCounts, code.toString());
+			}
+
+			if (card.data.isType(enums.type.TYPE_MONSTER)) {
+				Deck.increment(sideTypeCounts, "monster");
+			} else if (card.data.isType(enums.type.TYPE_SPELL)) {
+				Deck.increment(sideTypeCounts, "spell");
+			} else if (card.data.isType(enums.type.TYPE_TRAP)) {
+				Deck.increment(sideTypeCounts, "trap");
+			}
+		}
+
+		const archetypes = [];
+		for (const set in archetypeCounts) {
+			if (archetypeCounts[set] > 4) {
+				archetypes.push(set);
+			}
+		}
+
+		return {
+			nameCounts: {
+				main: mainNameCounts,
+				extra: extraNameCounts,
+				side: sideNameCounts
+			},
+			typeCounts: {
+				main: mainTypeCounts,
+				extra: extraTypeCounts,
+				side: sideTypeCounts
+			},
+			archetypes,
+			ydk: this.ydk,
+			url: this.url
+		};
 	}
 }
