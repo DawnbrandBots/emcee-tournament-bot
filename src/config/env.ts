01708825cc208b7c474fa9f564d759a1f1ff34b0
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-19 17:24:06 +0930

    Rename envvar to be github secret

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,10 +5,10 @@ if (process.env.DISCORD_TOKEN === undefined) {
 	throw new Error("Missing environment variable DISCORD_TOKEN!");
 }
 export const discordToken: string = process.env.DISCORD_TOKEN;
-if (process.env.GITHUB_TOKEN === undefined) {
-	throw new Error("Missing environment variable GITHUB_TOKEN!");
+if (process.env.OCTOKIT_TOKEN === undefined) {
+	throw new Error("Missing environment variable OCTOKIT_TOKEN!");
 }
-export const githubToken: string = process.env.GITHUB_TOKEN;
+export const octokitToken: string = process.env.OCTOKIT_TOKEN;
 if (process.env.MONGODB_URL === undefined) {
 	throw new Error("Missing environment variable MONGODB_URL!");
 }
--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,6 @@ import { YgoData } from "ygopro-data";
 import cardOpts from "./config/cardOpts.json";
 import dataOpts from "./config/dataOpts.json";
 import transOpts from "./config/transOpts.json";
-import { githubToken } from "./config/env";
+import { octokitToken } from "./config/env";
 
-export const data = new YgoData(cardOpts, transOpts, dataOpts, "./dbs", githubToken);
+export const data = new YgoData(cardOpts, transOpts, dataOpts, "./dbs", octokitToken);
