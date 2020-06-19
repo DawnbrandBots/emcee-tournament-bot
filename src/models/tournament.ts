b52fb050f8b6ecc9a13958ceead9459be194f255
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-06-18 23:49:18 -0400

    Tournament model: convert all Discord IDs to strings

--- src/models/tournament.ts
+++ src/models/tournament.ts
@@ -1,46 +1,48 @@
 import { Document, Schema, model } from "mongoose";
 
+type DiscordID = string;
+
 export interface Tournament extends Document {
 	name?: string;
 	description?: string;
-	organizers: number[];
-	owningDiscordServer: number;
-	discordChannels: number[];
+	organizers: DiscordID[];
+	owningDiscordServer: DiscordID;
+	discordChannels: DiscordID[];
 	status: "preparing" | "in progress" | "complete",
-	participantLimit: number;
+	participantLimit: DiscordID;
 	confirmedParticipants: {
-		discord: number,
+		discord: DiscordID,
 		deck: {
 			main: number[],
 			extra: number[],
 			side: number[]
 		}
 	};
-	pendingParticipants: number[];
+	pendingParticipants: DiscordID[];
 }
 
 export const TournamentSchema = new Schema({
 	name: { type: String, default: "" },
 	description: { type: String, default: "" },
-	organizers: { type: [Number], required: true },
-	owningDiscordServer: { type: Number, required: true },
-	discordChannels: { type: [Number], required: true },
+	organizers: { type: [String], required: true },
+	owningDiscordServer: { type: String, required: true },
+	discordChannels: { type: [String], required: true },
 	status: {
 		type: String,
 		enum: ["preparing", "in progress", "complete"],
 		required: true,
 		default: "preparing",
 	},
-	participantLimit: { type: Number, required: true, default: 0 },
+	participantLimit: { type: String, required: true, default: 0 },
 	confirmedParticipants: [{
-		discord: { type: Number, required: true },
+		discord: { type: String, required: true },
 		deck: {
 			main: { type: [Number], required: true },
 			extra: { type: [Number], required: true },
 			side: { type: [Number], required: true },
 		}
 	}],
-	pendingParticipants: { type: [Number], required: true, default: [] },
+	pendingParticipants: { type: [String], required: true, default: [] },
 });
 
 export const TournamentModel = model<Tournament>("Tournament", TournamentSchema);
