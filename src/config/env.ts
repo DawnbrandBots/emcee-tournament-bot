ed963a86911653c474c527ebe9f062fe2027ed0b
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-06-22 01:01:52 -0400

    env: add helper assertEnv

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,23 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-if (process.env.DISCORD_TOKEN === undefined) {
-	throw new Error("Missing environment variable DISCORD_TOKEN!");
+function assertEnv(envvar: string): string {
+	if (process.env[envvar] === undefined) {
+		throw new Error(`Missing environment variable ${envvar}`);
+	}
+	return process.env[envvar] as string;
 }
-export const discordToken: string = process.env.DISCORD_TOKEN;
-if (process.env.OCTOKIT_TOKEN === undefined) {
-	throw new Error("Missing environment variable OCTOKIT_TOKEN!");
-}
-export const octokitToken: string = process.env.OCTOKIT_TOKEN;
-if (process.env.MONGODB_URL === undefined) {
-	throw new Error("Missing environment variable MONGODB_URL!");
-}
-export const mongoDbUrl: string = process.env.MONGODB_URL;
-if (process.env.CHALLONGE_USERNAME === undefined) {
-	throw new Error("Missing environment variable CHALLONGE_TOKEN!");
-}
-export const challongeUsername: string = process.env.CHALLONGE_USERNAME;
-if (process.env.CHALLONGE_TOKEN === undefined) {
-	throw new Error("Missing environment variable CHALLONGE_TOKEN!");
-}
-export const challongeToken: string = process.env.CHALLONGE_TOKEN;
+
+export const discordToken: string      = assertEnv("DISCORD_TOKEN");
+export const octokitToken: string      = assertEnv("OCTOKIT_TOKEN");
+export const mongoDbUrl: string        = assertEnv("MONGODB_URL");
+export const challongeUsername: string = assertEnv("CHALLONGE_USERNAME");
+export const challongeToken: string    = assertEnv("CHALLONGE_TOKEN");
