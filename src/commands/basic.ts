d27ec9f86452ac0c53045784009077acf7872145
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-24 18:21:58 +0930

    Add documentation
    Closes #21

--- src/commands/basic.ts
+++ src/commands/basic.ts
@@ -21,3 +21,9 @@ export async function cancelTournament(msg: Message, args: string[]): Promise<vo
 	await tournament.finishTournament(msg.author.id, true);
 	await msg.channel.createMessage(`${doc.name} has successfully been cancelled.`);
 }
+
+export async function help(msg: Message): Promise<void> {
+	await msg.channel.createMessage(
+		"Emcee's documentation can be found at https://github.com/AlphaKretin/deck-parse-bot/wiki."
+	);
+}
