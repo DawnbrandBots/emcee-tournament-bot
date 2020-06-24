c16ea34a5a0a7382653d440dc4e71e9b2ff6ad02
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-24 16:18:20 +0930

    Add skeleton for creation of Tournament Organiser role on guild join

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,38 @@
-import Eris from "eris";
+import { Client, Message, GuildChannel } from "eris";
 import { discordToken } from "./config/env";
+import { MiscInternalError, AssertTextChannelError } from "./errors";
 
-export const bot = new Eris.Client(discordToken);
+export const bot = new Client(discordToken);
+
+const toRoles: { [guild: string]: string } = {}; // TODO: Persist
+
+export function getTORole(guildId: string): string {
+	if (!(guildId in toRoles)) {
+		throw new MiscInternalError(`No role for guild ${guildId}!`);
+	}
+	return toRoles[guildId];
+}
+
+export function getTORoleFromMessage(msg: Message): string {
+	const channel = msg.channel;
+	if (!(channel instanceof GuildChannel)) {
+		throw new AssertTextChannelError(channel.id);
+	}
+	const guild = channel.guild;
+	return getTORole(guild.id);
+}
+
+bot.on("guildCreate", guild => {
+	guild
+		.createRole({
+			name: "MC-TO",
+			color: 0x3498db
+		})
+		.then(role => {
+			toRoles[guild.id] = role.id;
+			// TODO: Log role creation once merged
+		})
+		.catch(e => {
+			console.error(e); // TODO: Log with winston once merged
+		});
+});
