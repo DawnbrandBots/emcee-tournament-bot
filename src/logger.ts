cbfb3fa42ef191c9120483290c46490b3da19c60
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-25 14:29:26 +0930

    logging: log errors as jsonlines, not pretty print

--- src/logger.ts
+++ src/logger.ts
@@ -2,9 +2,7 @@ import { createLogger, format, transports } from "winston";
 
 const logger = createLogger({
 	level: "info",
-	format: format.json({
-		space: 4
-	}),
+	format: format.json(),
 	defaultMeta: { service: "user-service" },
 	transports: [
 		new transports.File({ filename: "logs/error.log", level: "error" }),
