c6c684b2e4a6fe46a1cde2c02976ee3089375c01
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-25 13:22:34 +0930

    Extract TO role creation to helper function

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client, Message, GuildChannel } from "eris";
+import { Client, Message, GuildChannel, Guild, Role } from "eris";
 import { discordToken } from "./config/env";
 import { MiscInternalError, AssertTextChannelError } from "./errors";
 import logger from "./logger";
@@ -6,6 +6,22 @@ import logger from "./logger";
 export const bot = new Client(discordToken);
 
 const toRoles: { [guild: string]: string } = {};
+
+async function createTORole(guild: Guild, name: string): Promise<Role> {
+	const newRole = await guild.createRole(
+		{
+			name: name,
+			color: 0x3498db
+		},
+		"Auto-created by Emcee bot."
+	);
+	toRoles[guild.id] = newRole.id;
+	logger.log({
+		level: "verbose",
+		message: `TO role ${newRole.id} re-created in ${guild.id}.`
+	});
+	return newRole;
+}
 
 export async function getTORole(guildId: string): Promise<string> {
 	const guild = bot.guilds.get(guildId);
@@ -21,18 +37,7 @@ export async function getTORole(guildId: string): Promise<string> {
 		toRoles[guild.id] = role.id;
 		return role.id;
 	}
-	const newRole = await guild.createRole(
-		{
-			name: name,
-			color: 0x3498db
-		},
-		"Auto-created by Emcee bot."
-	);
-	toRoles[guild.id] = newRole.id;
-	logger.log({
-		level: "verbose",
-		message: `TO role ${newRole.id} re-created in ${guild.id}.`
-	});
+	const newRole = await createTORole(guild, name);
 	return newRole.id;
 }
 
@@ -46,25 +51,10 @@ export async function getTORoleFromMessage(msg: Message): Promise<string> {
 }
 
 bot.on("guildCreate", guild => {
-	guild
-		.createRole(
-			{
-				name: "MC-TO",
-				color: 0x3498db
-			},
-			"Auto-created by Emcee bot."
-		)
-		.then(role => {
-			toRoles[guild.id] = role.id;
-			logger.log({
-				level: "verbose",
-				message: `New TO role ${role.id} created in ${guild.id}.`
-			});
-		})
-		.catch(e => {
-			logger.log({
-				level: "error",
-				message: e.message
-			});
+	createTORole(guild, "MC-TO").catch(e => {
+		logger.log({
+			level: "error",
+			message: e.message
 		});
+	});
 });
