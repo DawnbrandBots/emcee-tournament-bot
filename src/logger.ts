ba8383207b22536702ad9e93f40be392413f18b3
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-25 14:42:41 +0930

    logging: Add timestamps

--- src/logger.ts
+++ src/logger.ts
@@ -2,12 +2,12 @@ import { createLogger, format, transports } from "winston";
 
 const logger = createLogger({
 	level: "info",
-	format: format.json(),
+	format: format.combine(format.json(), format.timestamp()),
 	defaultMeta: { service: "user-service" },
 	transports: [
 		new transports.File({ filename: "logs/error.log", level: "error" }),
-		new transports.Console({ level: "error", format: format.errors() }),
-		new transports.Console({ level: "info", format: format.simple() }),
+		new transports.Console({ level: "error", format: format.combine(format.errors(), format.timestamp()) }),
+		new transports.Console({ level: "info", format: format.combine(format.simple(), format.timestamp()) }),
 		new transports.File({ filename: "logs/verbose.log" })
 	]
 });
