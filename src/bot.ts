6309979e22fd9f1fdc9664546587a66a3c3cab6f
AlphaKretin <deewhydeeecks@gmail.com>
2020-06-25 13:25:56 +0930

    Extract TO role name to config option

--- src/bot.ts
+++ src/bot.ts
@@ -1,16 +1,17 @@
 import { Client, Message, GuildChannel, Guild, Role } from "eris";
 import { discordToken } from "./config/env";
 import { MiscInternalError, AssertTextChannelError } from "./errors";
+import { toRole } from "./config/config.json";
 import logger from "./logger";
 
 export const bot = new Client(discordToken);
 
 const toRoles: { [guild: string]: string } = {};
 
-async function createTORole(guild: Guild, name: string): Promise<Role> {
+async function createTORole(guild: Guild): Promise<Role> {
 	const newRole = await guild.createRole(
 		{
-			name: name,
+			name: toRole,
 			color: 0x3498db
 		},
 		"Auto-created by Emcee bot."
@@ -31,13 +32,12 @@ export async function getTORole(guildId: string): Promise<string> {
 	if (guild.id in toRoles) {
 		return toRoles[guild.id];
 	}
-	const name = "MC-TO";
-	const role = guild.roles.find(r => r.name === name);
+	const role = guild.roles.find(r => r.name === toRole);
 	if (role) {
 		toRoles[guild.id] = role.id;
 		return role.id;
 	}
-	const newRole = await createTORole(guild, name);
+	const newRole = await createTORole(guild);
 	return newRole.id;
 }
 
@@ -51,7 +51,7 @@ export async function getTORoleFromMessage(msg: Message): Promise<string> {
 }
 
 bot.on("guildCreate", guild => {
-	createTORole(guild, "MC-TO").catch(e => {
+	createTORole(guild).catch(e => {
 		logger.log({
 			level: "error",
 			message: e.message
