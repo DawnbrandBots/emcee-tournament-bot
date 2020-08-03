08dcf0eaaee6ef255ab5529654804efad32b8a1e
AlphaKretin <deewhydeeecks@gmail.com>
2020-08-03 10:51:56 +0930

    Implement PermissionController#addPublicChannel

--- src/controllers/permission.ts
+++ src/controllers/permission.ts
@@ -1,10 +1,25 @@
 import Controller, { DiscordWrapper } from "./controller";
+import { UserError } from "../errors";
+import { addAnnouncementChannel } from "../actions";
 
 export default class PermissionController extends Controller {
 	async addPublicChannel(discord: DiscordWrapper, args: string[]): Promise<void> {
 		// challonge ID, channel ID (optional, use current if not provided)
+		const tournament = await this.getTournament(discord, args);
+		let channelId = args[1];
+		if (channelId === undefined) {
+			channelId = discord.currentChannelId();
+		}
 		// avoid multiple addition
-		return;
+		if (tournament.publicChannels.includes(channelId)) {
+			throw new UserError(
+				`Channel <#${channelId}> is already registered as a public announcement channel for ${tournament.name}`
+			);
+		}
+		await addAnnouncementChannel(channelId, tournament.challongeId, discord.currentUser().id, "public");
+		discord.sendMessage(
+			`Channel <#${channelId}> successfully registered as a public announcement channel for ${tournament.name}`
+		);
 	}
 
 	async removePublicChannel(discord: DiscordWrapper, args: string[]): Promise<void> {
