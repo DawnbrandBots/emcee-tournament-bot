5763c6c08adfb0ce4de760e5be1829d5a03b624b
AlphaKretin <deewhydeeecks@gmail.com>
2020-08-03 10:54:44 +0930

    Implement add and remove Private Channel

--- src/controllers/permission.ts
+++ src/controllers/permission.ts
@@ -38,17 +38,43 @@ export default class PermissionController extends Controller {
 		discord.sendMessage(
 			`Channel <#${channelId}> successfully removed as a public announcement channel for ${tournament.name}`
 		);
-		return;
 	}
+
 	async addPrivateChannel(discord: DiscordWrapper, args: string[]): Promise<void> {
 		// challonge ID, channel ID (optional, use current if not provided)
+		const tournament = await this.getTournament(discord, args);
+		let channelId = args[1];
+		if (channelId === undefined) {
+			channelId = discord.currentChannelId();
+		}
 		// avoid multiple addition
-		return;
+		if (tournament.privateChannels.includes(channelId)) {
+			throw new UserError(
+				`Channel <#${channelId}> is already registered as a private announcement channel for ${tournament.name}`
+			);
+		}
+		await addAnnouncementChannel(channelId, tournament.challongeId, discord.currentUser().id, "private");
+		discord.sendMessage(
+			`Channel <#${channelId}> successfully registered as a private announcement channel for ${tournament.name}`
+		);
 	}
 
 	async removePrivateChannel(discord: DiscordWrapper, args: string[]): Promise<void> {
 		// challonge ID, channel ID (optional, use current if not provided)
-		return;
+		const tournament = await this.getTournament(discord, args);
+		let channelId = args[1];
+		if (channelId === undefined) {
+			channelId = discord.currentChannelId();
+		}
+		if (!tournament.privateChannels.includes(channelId)) {
+			throw new UserError(
+				`Channel <#${channelId}> not registered as a private announcement channel for ${tournament.name}`
+			);
+		}
+		await removeAnnouncementChannel(channelId, tournament.challongeId, discord.currentUser().id, "private");
+		discord.sendMessage(
+			`Channel <#${channelId}> successfully removed as a private announcement channel for ${tournament.name}`
+		);
 	}
 
 	async addTournamentHost(discord: DiscordWrapper, args: string[]): Promise<void> {
