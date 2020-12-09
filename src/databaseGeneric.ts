be1dd87e17b47edf05c15a502febdfa1ca369276
AlphaKretin <deewhydeeecks@gmail.com>
2020-12-09 20:08:01 +1030

    DB: Authenticate host and player

--- src/databaseGeneric.ts
+++ src/databaseGeneric.ts
@@ -1,3 +1,4 @@
+import { UnauthorisedHostError, UnauthorisedPlayerError } from "./errors";
 import { WebsiteTournament } from "./websiteGeneric";
 
 export interface DatabaseWrapper {
@@ -5,17 +6,39 @@ export interface DatabaseWrapper {
 	getTournament(tournamentId: string): Promise<DatabaseTournament>;
 }
 
+interface DatabasePlayer {
+	id: string;
+}
+
 // interface structure WIP as fleshed out command-by-command
 export interface DatabaseTournament {
 	id: string;
 	name: string;
 	description: string;
+	findHost: (id: string) => Promise<boolean>;
+	findPlayer: (id: string) => Promise<?DatabasePlayer>;
 }
 
 export class DatabaseInterface {
 	private db: DatabaseWrapper;
 	constructor(db: DatabaseWrapper) {
 		this.db = db;
+	}
+
+	public async authenticateHost(tournamentId: string, hostId: string): Promise<void> {
+		const tournament = await this.getTournament(tournamentId);
+		const auth = await tournament.findHost(hostId);
+		if (!auth) {
+			throw new UnauthorisedHostError(hostId, tournamentId);
+		}
+	}
+
+	public async authenticatePlayer(tournamentId: string, playerId: string): Promise<void> {
+		const tournament = await this.getTournament(tournamentId);
+		const player = await tournament.findPlayer(playerId);
+		if (!player) {
+			throw new UnauthorisedPlayerError(playerId, tournamentId);
+		}
 	}
 
 	public async createTournament(web: WebsiteTournament): Promise<DatabaseTournament> {
@@ -29,4 +52,5 @@ export class DatabaseInterface {
 	}
 }
 
+// TODO: Rename to "database" when a wrapper is implemented
 export const dummyDb = new DatabaseInterface();
