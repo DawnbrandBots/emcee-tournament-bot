a8ffd065521e09ce3e2adb5c5e668eeafeed42ab
AlphaKretin <deewhydeeecks@gmail.com>
2020-12-09 21:08:25 +1030

    Convert TournamentManager and CommandHandler to classes
    Input all interfaces they use so they are testable using mocks

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,215 +1,204 @@
 import { removeAnnouncementChannel } from "./actions";
 import { prettyPrint } from "./discordDeck";
 import { discord } from "./discordEris";
-import { DiscordMessageIn } from "./discordGeneric";
+import { DiscordInterface, DiscordMessageIn } from "./discordGeneric";
 import { UserError } from "./errors";
 import logger from "./logger";
-import {
-	addAnnouncementChannel,
-	addHost,
-	authenticateHost,
-	createTournament,
-	removeHost,
-	updateTournament,
-	openTournament,
-	startTournament,
-	authenticatePlayer,
-	submitScore,
-	nextRound,
-	listTournaments,
-	listPlayers,
-	getPlayerDeck,
-	dropPlayer,
-	syncTournament
-} from "./tournamentManager";
+import { tournamentManager, TournamentManager } from "./tournamentManager";
 
-async function commandHelp(msg: DiscordMessageIn): Promise<void> {
-	await msg.reply("Emcee's documentation can be found at https://github.com/AlphaKretin/emcee-tournament-bot/wiki.");
-}
+class CommandHandler {
+	private discord: DiscordInterface;
+	private tournamentManager: TournamentManager;
+	constructor(discord: DiscordInterface, tournamentManager: TournamentManager) {
+		this.discord = discord;
+		this.tournamentManager = tournamentManager;
 
-discord.registerCommand("help", commandHelp);
-discord.onPing(commandHelp);
+		this.discord.registerCommand("help", this.commandHelp);
+		this.discord.onPing(this.commandHelp);
+		this.discord.registerCommand("list", this.commandListTournaments);
+		this.discord.registerCommand("create", this.commandCreateTournament);
+		this.discord.registerCommand("update", this.commandUpdateTournament);
+		this.discord.registerCommand("addchannel", this.commandAddChannel);
+		this.discord.registerCommand("addchannel", this.commandRemoveChannel);
+		this.discord.registerCommand("addhost", this.commandAddHost);
+		this.discord.registerCommand("removehost", this.commandRemoveHost);
+		this.discord.registerCommand("open", this.commandOpenTournament);
+		this.discord.registerCommand("start", this.commandStartTournament);
+		this.discord.registerCommand("cancel", this.commandCancelTournament);
+		this.discord.registerCommand("score", this.commandSubmitScore);
+		this.discord.registerCommand("round", this.commandNextRound);
+		this.discord.registerCommand("players", this.commandListPlayers);
+		this.discord.registerCommand("deck", this.commandGetDeck);
+		this.discord.registerCommand("drop", this.commandDropPlayer);
+		this.discord.registerCommand("sync", this.commandSyncTournament);
+	}
+	private async commandHelp(msg: DiscordMessageIn): Promise<void> {
+		await msg.reply(
+			"Emcee's documentation can be found at https://github.com/AlphaKretin/emcee-tournament-bot/wiki."
+		);
+	}
 
-async function commandListTournaments(msg: DiscordMessageIn): Promise<void> {
-	await discord.authenticateTO(msg);
-	const list = await listTournaments();
-	await msg.reply(`\`\`\`\n${list}\`\`\``);
-}
+	private async commandListTournaments(msg: DiscordMessageIn): Promise<void> {
+		await this.discord.authenticateTO(msg);
+		const list = await this.tournamentManager.listTournaments();
+		await msg.reply(`\`\`\`\n${list}\`\`\``);
+	}
 
-discord.registerCommand("list", commandListTournaments);
+	private async commandCreateTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		await this.discord.authenticateTO(msg);
+		const [name, desc] = args;
+		const [id, url] = await this.tournamentManager.createTournament(name, desc);
+		logger.verbose(`New tournament created ${id} by ${msg.author}.`);
+		await msg.reply(
+			`Tournament ${name} created! You can find it at ${url}. For future commands, refer to this tournament by the id \`${id}\`.`
+		);
+	}
 
-async function commandCreateTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	await discord.authenticateTO(msg);
-	const [name, desc] = args;
-	const [id, url] = await createTournament(name, desc);
-	logger.verbose(`New tournament created ${id} by ${msg.author}.`);
-	await msg.reply(
-		`Tournament ${name} created! You can find it at ${url}. For future commands, refer to this tournament by the id \`${id}\`.`
-	);
-}
-
-discord.registerCommand("create", commandCreateTournament);
-
-async function commandUpdateTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id, name, desc] = args;
-	await authenticateHost(id, msg.author);
-	await updateTournament(id, name, desc);
-	logger.verbose(`Tournament ${id} updated with name ${name} and description ${desc} by ${msg.author}.`);
-	await msg.reply(`{
+	private async commandUpdateTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id, name, desc] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		await this.tournamentManager.updateTournament(id, name, desc);
+		logger.verbose(`Tournament ${id} updated with name ${name} and description ${desc} by ${msg.author}.`);
+		await msg.reply(`{
 		Tournament \`${id}\` updated! It now has the name ${name} and the given description.
 	}`);
-}
-
-discord.registerCommand("update", commandUpdateTournament);
-
-async function commandAddChannel(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const id = args[0];
-	let [, type, channel] = args;
-	await authenticateHost(id, msg.author);
-	if (!(type === "private")) {
-		type = "public";
 	}
-	// TODO: Parse actual channel mention. Just sketching out the structure for now.
-	if (!channel) {
-		channel = msg.channel;
+
+	private async commandAddChannel(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const id = args[0];
+		let [, type, channel] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		if (!(type === "private")) {
+			type = "public";
+		}
+		// TODO: Parse actual channel mention. Just sketching out the structure for now.
+		if (!channel) {
+			channel = msg.channel;
+		}
+		await this.tournamentManager.addAnnouncementChannel(id, channel, type as "public" | "private");
+		await this.discord.sendMessage(
+			`This channel added as a ${type} announcement channel for Tournament ${id}!`,
+			channel
+		);
+		await msg.reply(
+			`${this.discord.mentionChannel(channel)} added as a ${type} announcement channel for Tournament ${id}!`
+		);
 	}
-	await addAnnouncementChannel(id, channel, type as "public" | "private");
-	await discord.sendMessage(`This channel added as a ${type} announcement channel for Tournament ${id}!`, channel);
-	await msg.reply(`${discord.mentionChannel(channel)} added as a ${type} announcement channel for Tournament ${id}!`);
-}
 
-discord.registerCommand("addchannel", commandAddChannel);
-
-async function commandRemoveChannel(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const id = args[0];
-	let [, type, channel] = args;
-	await authenticateHost(id, msg.author);
-	if (!(type === "private")) {
-		type = "public";
+	private async commandRemoveChannel(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const id = args[0];
+		let [, type, channel] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		if (!(type === "private")) {
+			type = "public";
+		}
+		// TODO: Parse actual channel mention. Just sketching out the structure for now.
+		if (!channel) {
+			channel = msg.channel;
+		}
+		await removeAnnouncementChannel(id, channel, type as "public" | "private");
+		await this.discord.sendMessage(
+			`This channel removed as a ${type} announcement channel for Tournament ${id}!`,
+			channel
+		);
+		await msg.reply(
+			`${this.discord.mentionChannel(channel)} removed as a ${type} announcement channel for Tournament ${id}!`
+		);
 	}
-	// TODO: Parse actual channel mention. Just sketching out the structure for now.
-	if (!channel) {
-		channel = msg.channel;
+
+	private async commandAddHost(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		const newHost = this.discord.getMentionedUser(msg);
+		await this.tournamentManager.addHost(id, newHost);
+		await msg.reply(`${this.discord.mentionUser(newHost)} added as a host for Tournament ${id}!`);
 	}
-	await removeAnnouncementChannel(id, channel, type as "public" | "private");
-	await discord.sendMessage(`This channel removed as a ${type} announcement channel for Tournament ${id}!`, channel);
-	await msg.reply(
-		`${discord.mentionChannel(channel)} removed as a ${type} announcement channel for Tournament ${id}!`
-	);
-}
 
-discord.registerCommand("addchannel", commandRemoveChannel);
-
-async function commandAddHost(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	const newHost = discord.getMentionedUser(msg);
-	await addHost(id, newHost);
-	await msg.reply(`${discord.mentionUser(newHost)} added as a host for Tournament ${id}!`);
-}
-
-discord.registerCommand("addhost", commandAddHost);
-
-async function commandRemoveHost(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	const newHost = discord.getMentionedUser(msg);
-	await removeHost(id, newHost);
-	await msg.reply(`${discord.mentionUser(newHost)} removed as a host for Tournament ${id}!`);
-}
-
-discord.registerCommand("removehost", commandRemoveHost);
-
-async function commandOpenTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	await openTournament(id);
-	await msg.reply(`Tournament ${id} opened for registration!`);
-}
-
-discord.registerCommand("open", commandOpenTournament);
-
-async function commandStartTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	await startTournament(id);
-	await msg.reply(`Tournament ${id} successfully commenced!`);
-}
-
-discord.registerCommand("start", commandStartTournament);
-
-async function commandCancelTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	await startTournament(id);
-	await msg.reply(`Tournament ${id} successfully canceled.`);
-}
-
-discord.registerCommand("cancel", commandCancelTournament);
-
-// TODO infer tournamentId from tournament player is in? gotta make player-facing features as simple as possible
-async function commandSubmitScore(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id, score] = args;
-	await authenticatePlayer(id, msg.author);
-	const scores = score.split("-");
-	const scoreNums = scores.map(s => parseInt(s, 10));
-	if (scoreNums.length < 2) {
-		throw new UserError("Must provide score in format `#-#` e.g. `2-1`.");
+	private async commandRemoveHost(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		const newHost = this.discord.getMentionedUser(msg);
+		await this.tournamentManager.removeHost(id, newHost);
+		await msg.reply(`${this.discord.mentionUser(newHost)} removed as a host for Tournament ${id}!`);
 	}
-	await submitScore(id, msg.author, scoreNums[0], scoreNums[1]);
-	await msg.reply(
-		`Score of ${scoreNums[0]}-${scoreNums[1]} recorded for ${discord.mentionUser(msg.author)} in Tournament ${id}.`
-	);
+
+	private async commandOpenTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		await this.tournamentManager.openTournament(id);
+		await msg.reply(`Tournament ${id} opened for registration!`);
+	}
+
+	private async commandStartTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		await this.tournamentManager.startTournament(id);
+		await msg.reply(`Tournament ${id} successfully commenced!`);
+	}
+
+	private async commandCancelTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		await this.tournamentManager.startTournament(id);
+		await msg.reply(`Tournament ${id} successfully canceled.`);
+	}
+
+	// TODO infer tournamentId from tournament player is in? gotta make player-facing features as simple as possible
+	private async commandSubmitScore(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id, score] = args;
+		await this.tournamentManager.authenticatePlayer(id, msg.author);
+		const scores = score.split("-");
+		const scoreNums = scores.map(s => parseInt(s, 10));
+		if (scoreNums.length < 2) {
+			throw new UserError("Must provide score in format `#-#` e.g. `2-1`.");
+		}
+		await this.tournamentManager.submitScore(id, msg.author, scoreNums[0], scoreNums[1]);
+		await msg.reply(
+			`Score of ${scoreNums[0]}-${scoreNums[1]} recorded for ${this.discord.mentionUser(
+				msg.author
+			)} in Tournament ${id}.`
+		);
+	}
+
+	private async commandNextRound(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		const round = await this.tournamentManager.nextRound(id);
+		await msg.reply(`Tournament ${id} successfully progressed to round ${round}.`);
+	}
+
+	private async commandListPlayers(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		const list = await this.tournamentManager.listPlayers(id);
+		await msg.reply(list);
+	}
+
+	private async commandGetDeck(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		const player = this.discord.getMentionedUser(msg);
+		const deck = await this.tournamentManager.getPlayerDeck(id, player);
+		// TODO: Use player structs or add a name getter to the interface so we can name this file better.
+		const [message, attachment] = prettyPrint(deck, `${player}.ydk`);
+		await msg.reply(message, attachment);
+	}
+
+	private async commandDropPlayer(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		const player = this.discord.getMentionedUser(msg);
+		await this.tournamentManager.dropPlayer(id, player);
+		// TODO: Use player structs or add a name getter to the interface so we can reply better.
+		await msg.reply(`Player ${player} successfully dropped from Tournament ${id}.`);
+	}
+
+	private async commandSyncTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
+		const [id] = args;
+		await this.tournamentManager.authenticateHost(id, msg.author);
+		await this.tournamentManager.syncTournament(id);
+		await msg.reply(`Tournament ${id} database successfully synchronised with remote website.`);
+	}
 }
 
-discord.registerCommand("score", commandSubmitScore);
-
-async function commandNextRound(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	const round = await nextRound(id);
-	await msg.reply(`Tournament ${id} successfully progressed to round ${round}.`);
-}
-
-discord.registerCommand("round", commandNextRound);
-
-async function commandListPlayers(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	const list = await listPlayers(id);
-	await msg.reply(list);
-}
-
-discord.registerCommand("players", commandListPlayers);
-
-async function commandGetDeck(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	const player = discord.getMentionedUser(msg);
-	const deck = await getPlayerDeck(id, player);
-	// TODO: Use player structs or add a name getter to the interface so we can name this file better.
-	const [message, attachment] = prettyPrint(deck, `${player}.ydk`);
-	await msg.reply(message, attachment);
-}
-
-discord.registerCommand("deck", commandGetDeck);
-
-async function commandDropPlayer(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	const player = discord.getMentionedUser(msg);
-	await dropPlayer(id, player);
-	// TODO: Use player structs or add a name getter to the interface so we can reply better.
-	await msg.reply(`Player ${player} successfully dropped from Tournament ${id}.`);
-}
-
-discord.registerCommand("drop", commandDropPlayer);
-
-async function commandSyncTournament(msg: DiscordMessageIn, args: string[]): Promise<void> {
-	const [id] = args;
-	await authenticateHost(id, msg.author);
-	await syncTournament(id);
-	await msg.reply(`Tournament ${id} database successfully synchronised with remote website.`);
-}
-
-discord.registerCommand("sync", commandSyncTournament);
+const commandHandler = new CommandHandler(discord, tournamentManager);
--- src/tournamentManager.ts
+++ src/tournamentManager.ts
@@ -1,92 +1,106 @@
 import { Deck } from "ydeck";
-import { dummyDb } from "./databaseGeneric";
-import { dummyWebsite } from "./websiteGeneric";
+import { DatabaseInterface, dummyDb } from "./databaseGeneric";
+import { discord } from "./discordEris";
+import { DiscordInterface } from "./discordGeneric";
+import { dummyWebsite, WebsiteInterface } from "./websiteGeneric";
 
-export async function authenticateHost(tournamentId: string, hostId: string): Promise<void> {
-	await dummyDb.authenticateHost(tournamentId, hostId);
+export class TournamentManager {
+	private discord: DiscordInterface;
+	private database: DatabaseInterface;
+	private website: WebsiteInterface;
+	constructor(discord: DiscordInterface, database: DatabaseInterface, website: WebsiteInterface) {
+		this.discord = discord;
+		this.database = database;
+		this.website = website;
+	}
+	public async authenticateHost(tournamentId: string, hostId: string): Promise<void> {
+		await this.database.authenticateHost(tournamentId, hostId);
+	}
+
+	public async authenticatePlayer(tournamentId: string, playerId: string): Promise<void> {
+		await this.database.authenticatePlayer(tournamentId, playerId);
+	}
+
+	public async listTournaments(): Promise<string> {
+		const list = await this.database.listTournaments();
+		const text = list.map(t => `${t.name}\t${t.players.length} players`);
+		return text.join("\n");
+	}
+
+	public async createTournament(name: string, desc: string): Promise<[string, string]> {
+		const web = await this.website.createTournament(name, desc);
+		await this.database.createTournament(web);
+		return [web.id, web.url];
+	}
+	public async updateTournament(tournamentId: string, name: string, desc: string): Promise<void> {
+		// Update DB first because it performs an important check that might throw
+		await this.database.updateTournament(tournamentId, name, desc);
+		await this.website.updateTournament(tournamentId, name, desc);
+	}
+	public async addAnnouncementChannel(
+		tournamentId: string,
+		channel: string,
+		type: "public" | "private"
+	): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async removeAnnouncementChannel(
+		tournamentId: string,
+		channel: string,
+		type: "public" | "private"
+	): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async addHost(tournamentId: string, newHost: string): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async removeHost(tournamentId: string, newHost: string): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async openTournament(tournamentId: string): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async startTournament(tournamentId: string): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async cancelTournament(tournamentId: string): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async submitScore(
+		tournamentId: string,
+		playerId: string,
+		scorePlayer: number,
+		scoreOpp: number
+	): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async nextRound(tournamentId: string): Promise<number> {
+		throw new Error("Not implemented!");
+	}
+
+	public async listPlayers(tournamentId: string): Promise<string> {
+		throw new Error("Not implemented!");
+	}
+
+	public async getPlayerDeck(tournamentId: string, playerId: string): Promise<Deck> {
+		throw new Error("Not implemented!");
+	}
+
+	public async dropPlayer(tournamentId: string, playerId: string): Promise<void> {
+		throw new Error("Not implemented!");
+	}
+
+	public async syncTournament(tournamentId: string): Promise<void> {
+		throw new Error("Not implemented!");
+	}
 }
 
-export async function authenticatePlayer(tournamentId: string, playerId: string): Promise<void> {
-	await dummyDb.authenticatePlayer(tournamentId, playerId);
-}
-
-export async function listTournaments(): Promise<string> {
-	const list = await dummyDb.listTournaments();
-	const text = list.map(t => `${t.name}\t${t.players.length} players`);
-	return text.join("\n");
-}
-
-export async function createTournament(name: string, desc: string): Promise<[string, string]> {
-	const web = await dummyWebsite.createTournament(name, desc);
-	await dummyDb.createTournament(web);
-	return [web.id, web.url];
-}
-export async function updateTournament(tournamentId: string, name: string, desc: string): Promise<void> {
-	// Update DB first because it performs an important check that might throw
-	await dummyDb.updateTournament(tournamentId, name, desc);
-	await dummyWebsite.updateTournament(tournamentId, name, desc);
-}
-export async function addAnnouncementChannel(
-	tournamentId: string,
-	channel: string,
-	type: "public" | "private"
-): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function removeAnnouncementChannel(
-	tournamentId: string,
-	channel: string,
-	type: "public" | "private"
-): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function addHost(tournamentId: string, newHost: string): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function removeHost(tournamentId: string, newHost: string): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function openTournament(tournamentId: string): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function startTournament(tournamentId: string): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function cancelTournament(tournamentId: string): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function submitScore(
-	tournamentId: string,
-	playerId: string,
-	scorePlayer: number,
-	scoreOpp: number
-): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function nextRound(tournamentId: string): Promise<number> {
-	throw new Error("Not implemented!");
-}
-
-export async function listPlayers(tournamentId: string): Promise<string> {
-	throw new Error("Not implemented!");
-}
-
-export async function getPlayerDeck(tournamentId: string, playerId: string): Promise<Deck> {
-	throw new Error("Not implemented!");
-}
-
-export async function dropPlayer(tournamentId: string, playerId: string): Promise<void> {
-	throw new Error("Not implemented!");
-}
-
-export async function syncTournament(tournamentId: string): Promise<void> {
-	throw new Error("Not implemented!");
-}
+export const tournamentManager = new TournamentManager(discord, dummyDb, dummyWebsite);
