5ae59cfd140c3f84b60167473d720ea2037b668e
AlphaKretin <deewhydeeecks@gmail.com>
2020-12-11 09:26:08 +1030

    Fix binding issues
    Properly bind `this` on all Class method callbacks. Also rearrange handler instances to all be in main index file, imported tournamentHandler wasn't being properly created for some reason.

--- src/database/index.ts
+++
@@ -1,5 +0,0 @@
-import { DatabaseInterface } from "./interface";
-import { DatabaseWrapperMongoose } from "./mongoose";
-
-const mongoose = new DatabaseWrapperMongoose();
-export const database = new DatabaseInterface(mongoose);
--- src/tournamentManager/index.ts
+++
@@ -1,7 +0,0 @@
-import { database } from "../database";
-import { discord } from "../discord";
-import logger from "../logger";
-import { website } from "../website";
-import { TournamentManager } from "./TournamentManager";
-
-export const tournamentManager = new TournamentManager(discord, database, website, logger);
