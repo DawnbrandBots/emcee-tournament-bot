0e45637f504e6ac974c8bfee1f00224a4aa42ecf
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-12-20 18:48:20 -0500

    Partial attempt at testing Timer throughout the day

--- test/timer.ts
+++ test/timer.ts
@@ -1,12 +1,47 @@
-import { expect } from "chai";
+import chai, { expect } from "chai";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+import sinonTest from "sinon-test";
+import { DiscordInterface } from "../src/discord/interface";
 import { PersistentTimer } from "../src/timer";
+import logger from "../src/util/logger";
+import { DiscordWrapperMock } from "./mocks/discord";
+
+chai.use(sinonChai);
+const test = sinonTest(sinon);
+
+const discordMock = new DiscordWrapperMock();
+const discord = new DiscordInterface(discordMock, "mc!", logger);
 
 describe.skip("PersistentTimer is a timer", function () {
-	undefined;
+	it(
+		"initializes correctly",
+		test(async function (this: typeof sinon) {
+			// this.spy(Countdown);
+			// this.stub(Countdown, save);
+			const timer = await PersistentTimer.create(
+				discord,
+				new Date(Date.now() + 10000),
+				"1234567890",
+				"Test timer done",
+				1000
+			);
+			expect(timer.isActive()).to.be.true;
+			// expect save to have been called
+			// expect sendMessage to have been called with
+			// expect setInterval to have been called
+		})
+	);
+
+	it("calls tick and finishes");
+	it("can be aborted");
 });
 
 describe.skip("PersistentTimer is persisted", function () {
-	undefined;
+	// stub out Countdown.find
+	// stub out getConnection transaction
+	it("loads persisted timers and reinitializes");
+	it("prunes expired timers");
 });
 
 describe("formatTime", function () {
