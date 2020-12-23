9350ce2385e1cc66f90b69a71d6fc0b7cd20669e
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-12-22 21:57:16 -0500

    wtf I love proxyquire now

---
+++ test/formatTime.ts
@@ -0,0 +1,18 @@
+import { expect } from "chai";
+import { PersistentTimer } from "../src/timer";
+
+describe("formatTime", function () {
+	const tests = [
+		{ name: "basic format", milli: 10 * 60 * 1000 + 10 * 1000, str: "10:10" },
+		{ name: "zero pads", milli: 60 * 1000 + 1000, str: "01:01" },
+		{ name: "works with zero", milli: 0, str: "00:00" },
+		{ name: "ignores small fractions", milli: 1, str: "00:00" },
+		{ name: "rounds correctly", milli: 1500, str: "00:01" }
+	];
+
+	for (const test of tests) {
+		it(test.name, function () {
+			expect(PersistentTimer.formatTime(test.milli)).to.equal(test.str);
+		});
+	}
+});
--- test/timer.ts
+++ test/timer.ts
@@ -1,24 +1,32 @@
 import chai, { expect } from "chai";
+import proxyquire from "proxyquire";
 import sinon from "sinon";
 import sinonChai from "sinon-chai";
 import sinonTest from "sinon-test";
 import { DiscordInterface } from "../src/discord/interface";
-import { PersistentTimer } from "../src/timer";
 import logger from "../src/util/logger";
 import { DiscordWrapperMock } from "./mocks/discord";
 
 chai.use(sinonChai);
 const test = sinonTest(sinon);
+proxyquire.noPreserveCache();
 
 const discordMock = new DiscordWrapperMock();
 const discord = new DiscordInterface(discordMock, "mc!", logger);
 
-describe.skip("PersistentTimer is a timer", function () {
+const countdownEntityStub = {
+	save: sinon.stub()
+};
+const Countdown = sinon.stub().returns(countdownEntityStub);
+const { PersistentTimer } = proxyquire("../src/timer", {
+	"./database/orm": { Countdown }
+});
+
+describe("PersistentTimer is a timer", function () {
 	it(
 		"initializes correctly",
 		test(async function (this: typeof sinon) {
-			// this.spy(Countdown);
-			// this.stub(Countdown, save);
+			const sendMessageSpy = this.spy(discord, "sendMessage"); // TODO: replace when we mock Discord the same way
 			const timer = await PersistentTimer.create(
 				discord,
 				new Date(Date.now() + 10000),
@@ -27,8 +35,9 @@ describe.skip("PersistentTimer is a timer", function () {
 				1000
 			);
 			expect(timer.isActive()).to.be.true;
-			// expect save to have been called
-			// expect sendMessage to have been called with
+			expect(sendMessageSpy).to.have.been.calledWith("1234567890", "Time left in the round: `00:10`");
+			expect(Countdown).to.have.been.called;
+			expect(countdownEntityStub.save).to.have.been.called;
 			// expect setInterval to have been called
 		})
 	);
@@ -37,25 +46,9 @@ describe.skip("PersistentTimer is a timer", function () {
 	it("can be aborted");
 });
 
-describe.skip("PersistentTimer is persisted", function () {
+describe("PersistentTimer is persisted", function () {
 	// stub out Countdown.find
 	// stub out getConnection transaction
 	it("loads persisted timers and reinitializes");
 	it("prunes expired timers");
-});
-
-describe("formatTime", function () {
-	const tests = [
-		{ name: "basic format", milli: 10 * 60 * 1000 + 10 * 1000, str: "10:10" },
-		{ name: "zero pads", milli: 60 * 1000 + 1000, str: "01:01" },
-		{ name: "works with zero", milli: 0, str: "00:00" },
-		{ name: "ignores small fractions", milli: 1, str: "00:00" },
-		{ name: "rounds correctly", milli: 1500, str: "00:01" }
-	];
-
-	for (const test of tests) {
-		it(test.name, function () {
-			expect(PersistentTimer.formatTime(test.milli)).to.equal(test.str);
-		});
-	}
 });
