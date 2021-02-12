73260ce2a476f6373e69fd60e056cab83295903e
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-02-12 11:08:44 -0500

    Refactor: replace config.json with dotenv, pass config down from entry point

---
+++ src/config/index.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+function assertEnv(envvar: string): string {
+	if (process.env[envvar] === undefined) {
+		throw new Error(`Missing environment variable ${envvar}`);
+	}
+	// For some reason with indexed access, the cast is needed despite the if check
+	return process.env[envvar] as string;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type
+export function getConfig() {
+	return {
+		challongeUsername: assertEnv("CHALLONGE_USERNAME"),
+		challongeToken: assertEnv("CHALLONGE_TOKEN"),
+		defaultPrefix: assertEnv("EMCEE_DEFAULT_PREFIX"),
+		defaultTORole: assertEnv("EMCEE_DEFAULT_TO_ROLE"),
+		discordToken: assertEnv("DISCORD_TOKEN"),
+		octokitToken: assertEnv("OCTOKIT_TOKEN"),
+		postgresqlUrl: assertEnv("POSTGRESQL_URL")
+	};
+}
