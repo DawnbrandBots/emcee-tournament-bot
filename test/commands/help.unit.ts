309d8177f50052b4657415b850042ff31e035621
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-02-24 00:32:32 -0500

    Fix OCI labels and bake in Git SHA as EMCEE_REVISION for help command

--- test/commands/help.unit.ts
+++ test/commands/help.unit.ts
@@ -7,8 +7,6 @@ describe("command:help", function () {
 	it("responds with a help message", async () => {
 		msg.channel.createMessage = sinon.spy();
 		await command.executor(msg, [], support);
-		expect(msg.channel.createMessage).to.have.been.calledOnceWithExactly(
-			"Emcee's documentation can be found at https://github.com/AlphaKretin/emcee-tournament-bot/wiki."
-		);
+		expect(msg.channel.createMessage).to.have.been.calledOnce;
 	});
 });
