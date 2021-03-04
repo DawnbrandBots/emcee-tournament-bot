7d1b688c2d5cbbd3011dd31f86f9222c11d7cc4f
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-03-03 22:57:18 -0500

    forcedrop: implement as described

--- src/commands/forcedrop.ts
+++ src/commands/forcedrop.ts
@@ -1,5 +1,6 @@
 import { CommandDefinition } from "../Command";
-import { TournamentStatus } from "../database/interface";
+import { Participant } from "../database/orm";
+import { dropPlayerChallonge } from "../drop";
 import { reply } from "../util/discord";
 import { getLogger } from "../util/logger";
 
@@ -25,51 +26,51 @@ const command: CommandDefinition = {
 			);
 		}
 		log({ player, event: "attempt" });
-		const challongeId = await support.database.dropPlayer(id, player);
+		const participant = await Participant.findOne({ tournamentId: id, discordId: player });
 		const username = await support.discord.getRESTUsername(player);
-		log({ player, challongeId, username });
+		log({ player, exists: !!participant, challongeId: participant?.confirmed?.challongeId, username });
 		const name = username ? `<@${player}> (${username})` : who;
-		if (challongeId === null) {
+		if (!participant) {
 			await reply(msg, `${name} not found in **${tournament.name}**.`);
 			return;
 		}
-		if (challongeId !== undefined) {
-			// TODO: how does the failure path work here?
-			// Concede match if the tournament is ongoing
-			if (tournament.status === TournamentStatus.IPR) {
-				log({ player, challongeId, event: "concede" });
-				const match = await support.challonge.findClosedMatch(tournament.id, challongeId); // can also find open match
-				log({ player, challongeId, match });
-				// if there's no match, the dropping player had the natural bye
-				if (match) {
-					const oppChallonge = match.player1 === challongeId ? match.player2 : match.player1;
-					const opponent = await support.database.getPlayerByChallonge(oppChallonge, id).catch<undefined>();
-					log({ player, challongeId, oppId: opponent?.discordId, oppChallonge });
-					// if the opponent isn't in our database, the dropping player had an artificial bye
-					if (opponent) {
-						// for an open match, the dropping player concedes
-						if (match.open) {
-							await support.challonge.submitScore(tournament.id, match, oppChallonge, 2, 0);
-							log({ player, challongeId, event: "notify opponent" });
-							await support.discord
-								.sendDirectMessage(
-									opponent.discordId,
-									`Your opponent ${name} has dropped from the tournament, conceding this round to you. You don't need to submit a score for this round.`
-								)
-								.catch(logger.error);
-						} else {
-							// if the match is closed and the opponent has also dropped, the score needs to be amended to a tie
-							// TODO: keep in mind when we change to tracking dropped players
-							log({ player, challongeId, event: "double drop" });
-							await support.challonge.submitScore(tournament.id, match, oppChallonge, 0, 0);
-						}
-					}
+		if (participant.confirmed) {
+			if (
+				await dropPlayerChallonge(
+					id,
+					tournament.privateChannels,
+					tournament.status,
+					participant.confirmed.challongeId,
+					who,
+					log,
+					support.discord,
+					support.challonge,
+					support.database
+				)
+			) {
+				try {
+					await support.participantRole.ungrant(player, tournament);
+				} catch (error) {
+					logger.warn(error);
+					await reply(msg, `Failed to remove **${tournament.name}** participant role from ${name}.`).catch(
+						logger.error
+					);
 				}
+			} else {
+				await reply(
+					msg,
+					"Something went wrong. Please check private channels for problems and try again later."
+				);
+				return;
 			}
-			// Either way, take the confirmed participant off of Challonge and the Discord role
-			await support.challonge.removePlayer(tournament.id, challongeId);
-			log({ player, challongeId, event: "challonge" });
-			await support.participantRole.ungrant(player, tournament).catch(logger.error);
+		}
+		const confirmed = !!participant.confirmed;
+		try {
+			await participant.remove();
+		} catch (error) {
+			logger.error(error);
+			await reply(msg, "Something went wrong. Please check private channels for problems and try again later.");
+			return;
 		}
 		// Notify relevant parties as long as the participant existed
 		await support.discord
@@ -82,7 +83,7 @@ const command: CommandDefinition = {
 		}
 		await reply(
 			msg,
-			challongeId === undefined
+			confirmed
 				? `${name} was pending and dropped from **${tournament.name}**.`
 				: `${name} successfully dropped from **${tournament.name}**.`
 		);
