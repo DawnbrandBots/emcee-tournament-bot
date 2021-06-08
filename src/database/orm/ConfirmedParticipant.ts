921e79111b08a788bb54256518b5290c7e3282a8
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-06-07 20:24:09 -0400

    Add drop columns to ConfirmedParticipant

--- src/database/orm/ConfirmedParticipant.ts
+++ src/database/orm/ConfirmedParticipant.ts
@@ -27,6 +27,14 @@ export class ConfirmedParticipant extends BaseEntity {
 	@Column({ default: false })
 	hasBye!: boolean;
 
+	/// Did this participant forfeit a match and drop?
+	@Column({ default: 0 })
+	droppedDuringRound!: number;
+
+	/// Did this participant drop after their match concluded but before the next round started?
+	@Column({ default: 0 })
+	droppedAfterRound!: number;
+
 	/// Identical to Participant.tournament. Must always exist or this entity is meaningless.
 	@ManyToOne(() => ChallongeTournament, tournament => tournament.confirmed, { primary: true, onDelete: "CASCADE" })
 	@JoinColumn({ name: "tournamentId" })
