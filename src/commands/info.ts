904d2c32424e739c276eefd30abc7aa50f0dfe94
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-07-14 23:02:59 -0400

    Add command mc!info for in-server use by all

---
+++ src/commands/info.ts
@@ -0,0 +1,78 @@
+import { EmbedField } from "eris";
+import { CommandDefinition } from "../Command";
+import { ChallongeTournament } from "../database/orm";
+import { reply } from "../util/discord";
+import { getLogger } from "../util/logger";
+
+const logger = getLogger("command:info");
+
+const command: CommandDefinition = {
+	name: "info",
+	requiredArgs: ["id"],
+	executor: async (msg, args) => {
+		const [id] = args;
+		if (!msg.guildID) {
+			await reply(msg, `This can only be used in a server!`);
+			return;
+		}
+		const tournament = await ChallongeTournament.findOne({
+			tournamentId: id,
+			owningDiscordServer: msg.guildID
+		});
+		if (tournament) {
+			logger.verbose(
+				JSON.stringify({
+					channel: msg.channel.id,
+					message: msg.id,
+					user: msg.author.id,
+					tournament: id,
+					command: "info",
+					event: "found"
+				})
+			);
+			const fields: EmbedField[] = [
+				{ name: ":notepad_spiral: Format", value: tournament.format, inline: true },
+				{ name: ":hourglass: Status", value: tournament.status, inline: true },
+				{ name: ":ticket: Capacity", value: "256", inline: true },
+				{
+					name: ":tickets: Registered",
+					value: `**${tournament.confirmed.length}** participants`,
+					inline: true
+				}
+			];
+			const byes = tournament.confirmed
+				.filter(p => p.hasBye)
+				.map(p => `<@${p.discordId}>`)
+				.join(" ");
+			if (byes) {
+				fields.push({ name: ":sunglasses: Round 1 byes", value: byes, inline: true });
+			}
+			const hosts = tournament.hosts.map(snowflake => `<@${snowflake}>`).join(" ");
+			fields.push({ name: ":smile: Hosts", value: hosts, inline: true });
+			await reply(msg, {
+				embed: {
+					url: `https://challonge.com/${id}`,
+					title: `**${tournament.name}**`,
+					description: tournament.description,
+					fields,
+					footer: { text: "Tournament details as of request time" }
+				},
+				allowedMentions: { users: false }
+			});
+		} else {
+			logger.verbose(
+				JSON.stringify({
+					channel: msg.channel.id,
+					message: msg.id,
+					user: msg.author.id,
+					tournament: id,
+					command: "info",
+					event: "404"
+				})
+			);
+			await reply(msg, "No matching tournament in this server.");
+		}
+	}
+};
+
+export default command;
