49d2df8d48ca0f657692a78b7186e699f1fe678c
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-07-21 22:55:18 -0400

    Add tests

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,10 +1,40 @@
-import { EmbedField } from "eris";
+import { EmbedField, EmbedOptions } from "eris";
 import { CommandDefinition } from "../Command";
 import { ChallongeTournament } from "../database/orm";
 import { reply } from "../util/discord";
+import { UserError } from "../util/errors";
 import { getLogger } from "../util/logger";
 
 const logger = getLogger("command:info");
+
+export function createTournamentEmbed(tournament: ChallongeTournament): EmbedOptions {
+	const fields: EmbedField[] = [
+		{ name: ":ticket: Capacity", value: `${tournament.participantLimit || 256}`, inline: true },
+		{
+			name: ":tickets: Registered",
+			value: `**${tournament.confirmed.length}** participants`,
+			inline: true
+		},
+		{ name: ":notepad_spiral: Format", value: tournament.format, inline: true },
+		{ name: ":hourglass: Status", value: tournament.status, inline: true }
+	];
+	const byes = tournament.confirmed
+		.filter(p => p.hasBye)
+		.map(p => `<@${p.discordId}>`)
+		.join(" ");
+	if (byes) {
+		fields.push({ name: ":sunglasses: Round 1 byes", value: byes, inline: true });
+	}
+	const hosts = tournament.hosts.map(snowflake => `<@${snowflake}>`).join(" ");
+	fields.push({ name: ":smile: Hosts", value: hosts, inline: true });
+	return {
+		url: `https://challonge.com/${tournament.tournamentId}`,
+		title: `**${tournament.name}**`,
+		description: tournament.description,
+		fields,
+		footer: { text: "Tournament details as of request time" }
+	};
+}
 
 const command: CommandDefinition = {
 	name: "info",
@@ -12,8 +42,7 @@ const command: CommandDefinition = {
 	executor: async (msg, args) => {
 		const [id] = args;
 		if (!msg.guildID) {
-			await reply(msg, `This can only be used in a server!`);
-			return;
+			throw new UserError("This can only be used in a server!");
 		}
 		const tournament = await ChallongeTournament.findOne({
 			tournamentId: id,
@@ -30,33 +59,9 @@ const command: CommandDefinition = {
 					event: "found"
 				})
 			);
-			const fields: EmbedField[] = [
-				{ name: ":ticket: Capacity", value: `${tournament.participantLimit || 256}`, inline: true },
-				{
-					name: ":tickets: Registered",
-					value: `**${tournament.confirmed.length}** participants`,
-					inline: true
-				},
-				{ name: ":notepad_spiral: Format", value: tournament.format, inline: true },
-				{ name: ":hourglass: Status", value: tournament.status, inline: true }
-			];
-			const byes = tournament.confirmed
-				.filter(p => p.hasBye)
-				.map(p => `<@${p.discordId}>`)
-				.join(" ");
-			if (byes) {
-				fields.push({ name: ":sunglasses: Round 1 byes", value: byes, inline: true });
-			}
-			const hosts = tournament.hosts.map(snowflake => `<@${snowflake}>`).join(" ");
-			fields.push({ name: ":smile: Hosts", value: hosts, inline: true });
+			const embed = createTournamentEmbed(tournament);
 			await reply(msg, {
-				embed: {
-					url: `https://challonge.com/${id}`,
-					title: `**${tournament.name}**`,
-					description: tournament.description,
-					fields,
-					footer: { text: "Tournament details as of request time" }
-				},
+				embed,
 				allowedMentions: { users: false }
 			});
 		} else {
