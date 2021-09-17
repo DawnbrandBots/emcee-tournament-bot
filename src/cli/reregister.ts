0198117748dc1da0fa21df15426d87d2f42c880c
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-09-16 21:37:23 -0400

    Add CLI reregister command for an emergency restore

---
+++ src/cli/reregister.ts
@@ -0,0 +1,38 @@
+import { Client } from "discord.js";
+import dotenv from "dotenv";
+import { getConnection } from "typeorm";
+import { ConfirmedParticipant, initializeConnection } from "../database/orm";
+import { WebsiteWrapperChallonge } from "../website/challonge";
+import { WebsiteInterface } from "../website/interface";
+dotenv.config();
+
+const args = process.argv.slice(2);
+if (!args.length) {
+	console.error("Missing target tournament argument!");
+	process.exit(1);
+}
+
+const challonge = new WebsiteInterface(
+	new WebsiteWrapperChallonge(`${process.env.CHALLONGE_USERNAME}`, `${process.env.CHALLONGE_TOKEN}`)
+);
+
+const client = new Client({ intents: [] });
+
+(async () => {
+	await initializeConnection(`${process.env.POSTGRESQL_URL}`);
+	await client.login();
+	const participants = await ConfirmedParticipant.find({ tournamentId: args[0] });
+	console.log(`${participants.length} participants to process.`);
+	for (const participant of participants) {
+		console.group(`Processing <@${participant.discordId}>...`);
+		const user = await client.users.fetch(participant.discordId);
+		console.log(`<@${participant.discordId}> is ${user.tag}`);
+		const challongeId = await challonge.registerPlayer(args[0], user.tag, participant.discordId);
+		console.log(`<@${participant.discordId}> re-registered as ${challongeId}`);
+		participant.challongeId = challongeId;
+		await participant.save();
+		console.groupEnd();
+	}
+	client.destroy();
+	await getConnection().close();
+})();
