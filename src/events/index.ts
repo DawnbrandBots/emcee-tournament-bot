2e9c8303bba739631498e0e41115222cef7cbdab
kevinlul <6320810+kevinlul@users.noreply.github.com>
2023-01-05 20:38:39 -0500

    Bump essentials and TS

--- src/events/index.ts
+++ src/events/index.ts
@@ -19,9 +19,9 @@ export function registerEvents(bot: Client, prefix: string, support: CommandSupp
 	bot.on("shardReady", shard => logger.notify(`Shard ${shard} ready`));
 	bot.on("shardReconnecting", shard => logger.info(`Shard ${shard} reconnecting`));
 	bot.on("shardResume", (shard, replayed) => logger.info(`Shard ${shard} resumed: ${replayed} events replayed`));
-	bot.on("shardDisconnect", (event, shard) =>
-		logger.notify(`Shard ${shard} disconnected (${event.code},${event.wasClean}): ${event.reason}`)
-	);
+	bot.on("shardDisconnect", (event, shard) => {
+		logger.notify(`Shard ${shard} disconnected (${event.code},${event.wasClean}): ${event.reason}`);
+	});
 	bot.on("shardError", (error, shard) => logger.error(`Shard ${shard} error:`, error));
 	bot.on("guildDelete", guild => logger.notify(`Guild delete: ${serializeServer(guild)}`));
 	bot.on("guildCreate", guildCreate.makeHandler(support.organiserRole));
