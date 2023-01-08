20a51a089ab9d5e8de8da01fa91a7c2681373728
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2023-01-08 16:54:53 -0500

    Bump Node.js to v18, TypeScript to 4.9 (#438)

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,10 +1,6 @@
 import { Guild } from "discord.js";
 
 export function serializeServer(server: Guild): string {
-	if ("name" in server) {
-		const createdAt = new Date(server.createdAt).toISOString();
-		return `${server.name} (${server.id}) [${server.memberCount}] ${createdAt} by <@${server.ownerId}>`;
-	} else {
-		return `${server.id}`;
-	}
+	const createdAt = new Date(server.createdAt).toISOString();
+	return `${server.name} (${server.id}) [${server.memberCount}] ${createdAt} by <@${server.ownerId}>`;
 }
