3d818097952d688cc72fa7fc926c93be79b6e562
AlphaKretin <deewhydeeecks@gmail.com>
2023-01-27 10:41:26 +1030

    Add player list to csv command

--- src/slash/csv.ts
+++ src/slash/csv.ts
@@ -5,6 +5,7 @@ import { AutocompletableCommand } from "../SlashCommand";
 import { getLogger, Logger } from "../util/logger";
 import { authenticateHost, autocompleteTournament, tournamentOption } from "./database";
 import * as csv from "@fast-csv/format";
+import { username } from "../util/discord";
 
 export class CsvCommand extends AutocompletableCommand {
 	#logger = getLogger("command:csv");
@@ -19,7 +20,17 @@ export class CsvCommand extends AutocompletableCommand {
 			.setDescription("Generate a CSV of participant details.")
 			.setDMPermission(false)
 			.setDefaultMemberPermissions(0)
-			.addStringOption(tournamentOption) // TODO: format option for pie/players
+			.addStringOption(tournamentOption)
+			.addStringOption(option =>
+				option
+					.setName("type")
+					.setDescription("The type of data to display.")
+					.setRequired(true)
+					.addChoices(
+						{ name: "Pie Chart", value: "Pie Chart" },
+						{ name: "Player List", value: "Player List" }
+					)
+			)
 			.toJSON();
 	}
 
@@ -40,14 +51,33 @@ export class CsvCommand extends AutocompletableCommand {
 			return;
 		}
 
-		const themes = tournament.participants
-			.filter(p => p.deck?.approved)
-			.map(p => p.deck?.label || "No theme")
-			.reduce((map, theme) => map.set(theme, (map.get(theme) || 0) + 1), new Map<string, number>());
-		const file = await csv.writeToBuffer([["Theme", "Count"], ...themes.entries()]);
+		const pie = interaction.options.getString("type", true) === "Pie Chart";
+
+		let file: Buffer;
+
+		if (pie) {
+			const themes = tournament.participants
+				.filter(p => p.deck?.approved)
+				.map(p => p.deck?.label || "No theme")
+				.reduce((map, theme) => map.set(theme, (map.get(theme) || 0) + 1), new Map<string, number>());
+			file = await csv.writeToBuffer([["Theme", "Count"], ...themes.entries()]);
+		} else {
+			const players = tournament.participants
+				.filter(p => p.deck?.approved)
+				.map(async player => {
+					const tag = (await username(interaction.client, player.discordId)) || player.discordId;
+					return {
+						Player: tag,
+						Theme: player.deck?.label || "No theme"
+					};
+				});
+			file = await csv.writeToBuffer(await Promise.all(players), { headers: true });
+		}
 
 		await interaction.reply({
-			content: `A list of deck themes in ${tournament.name} with their counts is attached.`,
+			content: `A list of ${pie ? "deck themes" : "players"} in ${tournament.name} with their ${
+				pie ? "counts" : "deck themes"
+			} is attached.`,
 			files: [{ attachment: file, name: `${tournament.name}.csv` }]
 		});
 	}
