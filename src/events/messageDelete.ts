82cd5f11519e432c9cfbe1e491db854d693a7033
AlphaKretin <deewhydeeecks@gmail.com>
2023-02-01 09:59:14 +1030

    Add missing await
    Old challonge function was implicitly awaited by being an event handler before but is inline now

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -8,7 +8,7 @@ const logger = getLogger("messageDelete");
 
 export function makeHandler(bot: Client, support: CommandSupport): (msg: Message | PartialMessage) => Promise<void> {
 	return async function messageDelete(msg: Message | PartialMessage): Promise<void> {
-		support.database.cleanRegistration(msg.channelId, msg.id);
+		await support.database.cleanRegistration(msg.channelId, msg.id);
 		try {
 			// don't need the inner join on tournament because even - especially - mid-tournament, a deck dodger should be dropped
 			const deletedDecks = await ManualDeckSubmission.find({
