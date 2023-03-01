c872e1db6e9dd6f962f5fe5ef5041452f199d8de
AlphaKretin <deewhydeeecks@gmail.com>
2023-03-01 15:30:39 +1030

    Update /drop
    Make tournament parameter optional and infer if missing

--- src/slash/drop.ts
+++ src/slash/drop.ts
@@ -1,9 +1,14 @@
 import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
-import { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-import { ManualTournament } from "../database/orm";
+import {
+	AutocompleteInteraction,
+	chatInputApplicationCommandMention,
+	ChatInputCommandInteraction,
+	SlashCommandBuilder
+} from "discord.js";
+import { ManualParticipant, ManualTournament } from "../database/orm";
 import { AutocompletableCommand } from "../SlashCommand";
 import { getLogger, Logger } from "../util/logger";
-import { authenticatePlayer, autocompleteTournament, dropPlayer, tournamentOption } from "./database";
+import { authenticatePlayer, autocompleteTournament, dropPlayer } from "./database";
 
 export class DropCommand extends AutocompletableCommand {
 	#logger = getLogger("command:drop");
@@ -13,13 +18,22 @@ export class DropCommand extends AutocompletableCommand {
 	}
 
 	static override get meta(): RESTPostAPIApplicationCommandsJSONBody {
-		return new SlashCommandBuilder()
-			.setName("drop")
-			.setDescription("Remove yourself from a tournament.")
-			.setDMPermission(false)
-			.setDefaultMemberPermissions(0)
-			.addStringOption(tournamentOption)
-			.toJSON();
+		return (
+			new SlashCommandBuilder()
+				.setName("drop")
+				.setDescription("Remove yourself from a tournament.")
+				.setDMPermission(false)
+				.setDefaultMemberPermissions(0)
+				// explicitly recreate tournamentOption to make it optional
+				.addStringOption(option =>
+					option
+						.setName("tournament")
+						.setDescription("The name of the tournament to edit.")
+						.setRequired(false)
+						.setAutocomplete(true)
+				)
+				.toJSON()
+		);
 	}
 
 	protected override get logger(): Logger {
@@ -32,7 +46,37 @@ export class DropCommand extends AutocompletableCommand {
 
 	protected override async execute(interaction: ChatInputCommandInteraction<"cached">): Promise<void> {
 		await interaction.deferReply();
-		const tournamentName = interaction.options.getString("tournament", true);
+		const tournamentName = interaction.options.getString("tournament");
+		if (!tournamentName) {
+			const players = await ManualParticipant.find({
+				where: { discordId: interaction.user.id },
+				relations: ["tournament"]
+			});
+
+			if (players.length < 1) {
+				await interaction.editReply(`You are not in any tournaments.`);
+				return;
+			}
+
+			if (players.length > 1) {
+				if (!interaction.guild.commands.cache.size) {
+					await interaction.guild.commands.fetch();
+				}
+				const id = interaction.guild.commands.cache.find(command => command.name === "drop")?.id || "";
+				const mention = chatInputApplicationCommandMention("forcedrop", id);
+				await interaction.editReply(
+					`You are in multiple tournaments. Use the \`tournament\` option in ${mention} to specify.`
+				);
+				return;
+			}
+
+			const player = players[0];
+
+			const tournament = player.tournament;
+
+			await dropPlayer(tournament, player, interaction.member, interaction);
+			return;
+		}
 		const tournament = await ManualTournament.findOneOrFail({
 			where: { name: tournamentName },
 			relations: ["participants"]
